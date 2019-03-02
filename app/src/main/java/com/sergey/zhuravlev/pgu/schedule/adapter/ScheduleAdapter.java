87e0e46c816a415beae47c965bddb3a042f85ab4
dergey <dergey@gmail.com>
2019-03-02 14:13:17 +0300

    Fixed reloading of the schedule, when repeating groups were created.

--- app/src/main/java/com/sergey/zhuravlev/pgu/schedule/adapter/ScheduleAdapter.java
+++ app/src/main/java/com/sergey/zhuravlev/pgu/schedule/adapter/ScheduleAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.view.ViewGroup;
 
 import com.sergey.zhuravlev.pgu.schedule.fragment.GroupFragment;
 import com.sergey.zhuravlev.pgu.schedule.model.Group;
@@ -27,11 +29,28 @@ public class ScheduleAdapter extends FragmentPagerAdapter {
 
     public void setSchedule(Schedule schedule) {
         this.schedule = schedule;
-//        if (!tabs.isEmpty()) throw new Exception("Cant load 2 step");
+        this.clear();
         for (Group group : schedule.getGroups()) {
             this.tabs.add(GroupFragment.getInstance(context, group, schedule.getGroupClassworks(group)));
         }
         this.notifyDataSetChanged();
+    }
+
+    public void clear() {
+        for (int i = 0; i < tabs.size(); i++) {
+            this.destroyItem(null, i, tabs.get(i));
+        }
+        tabs.clear();
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        if (position >= getCount()) {
+            FragmentManager manager = ((Fragment) object).getFragmentManager();
+            FragmentTransaction trans = manager.beginTransaction();
+            trans.remove((Fragment) object);
+            trans.commit();
+        }
     }
 
     @Override
